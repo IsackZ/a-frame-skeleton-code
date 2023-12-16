8916269f27549b97f987d5601f14d92347fac1c3
IsackZ Z <zhangisaac32@gmail.com>
2023-12-16 04:27:36 +0000

    commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const SkyColor = document.getElementById("SkyColor")
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+SkyColor.setAttribute(getRandomColor())
